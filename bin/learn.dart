a04985fe92f82688d57b2d0f08b85ab83c7f44cf
u006E10011 <zaaaaazzzazzaz@gmail.com>
2026-06-12 17:41:10 +0800

    delete garbage

--- bin/learn.dart
+++
@@ -1,5 +0,0 @@
-import 'package:learn/learn.dart' as learn;
-
-void main(List<String> arguments) {
-  print('Hello world: ${learn.calculate()}!');
-}
--- test/learn_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:learn/learn.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
